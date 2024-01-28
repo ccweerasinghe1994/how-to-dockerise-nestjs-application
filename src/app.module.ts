05360a28058144934e73747d07f83ce473b3da6c
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-01-28 10:38:48 +0530

    Add TypeORM configuration to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'mysql_db',
+    port: 3306,
+    database: 'nestjs_api_dev',
+    entities:[],
+    username: 'nestjs_api_dev',
+    password: 'nestjs_api_dev',
+    synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
